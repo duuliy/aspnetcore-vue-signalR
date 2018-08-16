b1337a36beeebe6c080215c0c1b18c5a0ad27681
Yuhan zhong <715181149@qq.com>
2018-08-16 13:50:37 +0800

    Update home-page.vue

--- ClientApp/components/home-page.vue
+++ ClientApp/components/home-page.vue
@@ -6,7 +6,6 @@
             <li><a href="https://get.asp.net/">ASP.NET Core</a> and <a href="https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx">C#</a>                    for cross-platform server-side code</li>
             <li><a href="https://vuejs.org/">Vue.js</a> and ES6 using <a href="https://babeljs.io/">BableJs</a> for client-side code</li>
             <li><a href="https://webpack.github.io/">Webpack</a> for building and bundling client-side resources</li>
-            <li><a href="http://getbootstrap.com/">Bootstrap</a> for layout and styling</li>
             <li><a href="https://github.com/aspnet/SignalR">SignalR Alpha</a> for realtime socket based communication between clients and server.</a></li>
             <li><a href="api/SampleData/WeatherForecasts">API sample data</a> from the dotnet controller</li>
         </ul>
