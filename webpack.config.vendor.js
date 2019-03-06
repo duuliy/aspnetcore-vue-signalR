3606f8b3f98ad83325aada043e8ab5af4a2c44ef
Yuhan zhong <715181149@qq.com>
2019-03-06 18:52:22 +0800

    Update webpack.config.vendor.js

--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -1,7 +1,7 @@
-﻿const path = require('path');
+const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');  //将 css 文件分离出来,提取css文件到一个独立的文件中去
+var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');  //使用这个插件压缩css，主要是因为，对于不同组件中相同的css可以剔除一部分
 
 module.exports = (env) => {
     const extractCSS = new ExtractTextPlugin('vendor.css');
@@ -35,15 +35,15 @@ module.exports = (env) => {
                 }
             }),
             new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
-            new webpack.DllPlugin({
+            new webpack.DllPlugin({     //将一些不做修改的依赖文件，提前打包，这样我们开发代码发布的时候就不需要再对这部分代码进行打包。manifest。
                 path: path.join(__dirname, 'wwwroot', 'dist', '[name]-manifest.json'),
                 name: '[name]_[hash]'
             }),
-            new webpack.DefinePlugin({
+            new webpack.DefinePlugin({  // 一个可以插入 html 并且创建新的 .html 文件的插件
                 'process.env.NODE_ENV': isDevBuild ? '"development"' : '"production"'
             })
         ].concat(isDevBuild ? [] : [
-            new webpack.optimize.UglifyJsPlugin()
+            new webpack.optimize.UglifyJsPlugin()   //删除注释等并压缩
         ])
     }];
 };
